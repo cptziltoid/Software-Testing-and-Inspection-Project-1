5c0bc7e4186e88ddaca18059d8591204901c8f62
Gabriel <gabriel.francischini@icloud.com>
2020-05-07 17:52:02 -0300

    Update TestGameOfLife.java
    Só um caso de teste mais robusto

--- Game of Life/src/TestGameOfLife.java
+++ Game of Life/src/TestGameOfLife.java
@@ -22,6 +22,20 @@ public class TestGameOfLife {
 				 			{0, 0, 0, 0, 0, 0},    
 				 			{0, 0, 0, 0, 0, 0}}; 
 		
+		int[][] test2 ={{1, 0, 0, 0, 0, 1},    
+				 		{1, 0, 0, 0, 0, 1},    
+				 		{0, 0, 1, 0, 0, 1},    
+				 		{0, 0, 0, 0, 0, 1},    
+				 		{1, 1, 1, 1, 1, 1},    
+				 		{0, 1, 0, 1, 1, 1}};
+
+		int[][] expected2 ={{0, 0, 0, 0, 0, 0},    
+				 			{0, 1, 0, 0, 1, 1},    
+				 			{0, 0, 0, 0, 1, 1},    
+				 			{0, 0, 0, 0, 0, 1},    
+				 			{1, 1, 0, 0, 0, 0},    
+				 			{1, 1, 0, 0, 0, 1}};
+		
 		GameOfLife game = new GameOfLife(test1, rows, columns);
 		int[][] test1Output = game.analyzeTable();
 		assertArrayEquals(test1Output, expected1);
